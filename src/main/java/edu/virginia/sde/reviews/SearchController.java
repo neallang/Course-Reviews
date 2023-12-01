b5bef362953171188b6ff012fa60745571048108
connolly-woo <egs6nf@virginia.edu>
2023-11-30 22:58:10 -0500

    reconfigured search-screen.fxml with Scene Builder and created SearchController class

---
+++ src/main/java/edu/virginia/sde/reviews/SearchController.java
@@ -0,0 +1,20 @@
+package edu.virginia.sde.reviews;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+
+public class SearchController {
+    @FXML
+    private Label messageLabel;
+
+    public void handleCourseIDButton() {
+        messageLabel.setText("You pressed the button!");
+    }
+    public void handleCourseNameButton() {
+        messageLabel.setText("You pressed the button!");
+    }
+    public void handleCourseMnemonicButton() {
+        messageLabel.setText("You pressed the button!");
+    }
+}
