6921761c86f60ffdd3c376865590118dfdd4ecc3
connolly-woo <egs6nf@virginia.edu>
2023-12-03 21:45:26 -0500

    Created My Reviews scene and controller and application

---
+++ src/main/java/edu/virginia/sde/reviews/MyReviewsScreenApplication.java
@@ -0,0 +1,21 @@
+package edu.virginia.sde.reviews;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MyReviewsScreenApplication extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("my-reviews.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("My Reviews");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
