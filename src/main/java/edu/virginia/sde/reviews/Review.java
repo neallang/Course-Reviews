d5c031279c085ef2325ebfb553e53d96c5af1fed
neallang <neal_langhorne@yahoo.com>
2023-12-04 20:47:23 -0500

    updated review

--- src/main/java/edu/virginia/sde/reviews/Review.java
+++ src/main/java/edu/virginia/sde/reviews/Review.java
@@ -1,14 +1,21 @@
 package edu.virginia.sde.reviews;
 
+
+import java.sql.Timestamp;
+
 public class Review {
     private int userID;
     private int courseID;
     private String reviewText;
+    private int rating;
+    private Timestamp timeStamp;
 
-    public Review(int userID, int courseID, String reviewText) {
+    public Review(int userID, int courseID, String reviewText, int rating, Timestamp timeStamp) {
         this.userID = userID;
         this.courseID = courseID;
         this.reviewText = reviewText;
+        this.rating = rating;
+        this.timeStamp = timeStamp;
     }
 
     public int getUserID() {
