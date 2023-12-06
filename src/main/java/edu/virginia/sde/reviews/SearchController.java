1fb007678b9ae47278f2fc82405e28c6ca500f9a
connolly-woo <egs6nf@virginia.edu>
2023-12-05 23:16:04 -0500

    lost all my changes then got them all back :D (?)

--- src/main/java/edu/virginia/sde/reviews/SearchController.java
+++ src/main/java/edu/virginia/sde/reviews/SearchController.java
@@ -1,13 +1,5 @@
 package edu.virginia.sde.reviews;
 
-import javafx.beans.InvalidationListener;
-import javafx.beans.binding.ObjectBinding;
-import javafx.beans.property.ReadOnlyStringWrapper;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableObjectValue;
-import javafx.beans.value.ObservableStringValue;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -19,7 +11,7 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
+
 import java.sql.*;
 import javafx.collections.*;
 
@@ -57,26 +49,6 @@ public class SearchController {
 
     DatabaseDriver databaseDriver = new DatabaseDriver("appDatabase.sqlite");
 
-    public void handleCourseIDButton() {
-        messageLabel.setText("You pressed the button!");
-    }
-    public void handleCourseNameButton() {
-        messageLabel.setText("You pressed the button!");
-    }
-    public void handleCourseMnemonicButton() {
-        messageLabel.setText("You pressed the button!");
-    }
-
-//    private Button viewButton() throws IOException{
-//        Button button = new Button("View");
-//        button.setOnAction(event -> {
-//            try{
-//                switchToMyReviews(event);
-//            }catch (IOException e){
-//                e.printStackTrace();
-//            }
-//        });
-//    }
     public void switchToMyReviews(javafx.event.ActionEvent actionEvent) throws IOException {
         root = FXMLLoader.load(new File("src/main/resources/edu/virginia/sde/reviews/my-reviews.fxml").toURI().toURL());
 
@@ -118,56 +90,9 @@ public class SearchController {
         numCol.setCellValueFactory(new PropertyValueFactory<Course, String>("courseNumber"));
         subjectCol.setCellValueFactory(new PropertyValueFactory<Course, String>("department"));
         titleCol.setCellValueFactory(new PropertyValueFactory<Course, String>("title"));
-//        reviewsCol.setCellFactory(view -> new TableCell<Button, Void>(){
-//            private Button viewButton() throws IOException{
-//                Button button = new Button("View");
-//                button.setOnAction(event -> {
-//                    try{
-//                        switchToMyReviews(event);
-//                    }catch (IOException e){
-//                        e.printStackTrace();
-//                    }
-//                });
-//            }
-//        });
 
         displayCourses.setItems(observableCourses);
     }
 
-//    @FXML
-//    void searchCourses(javafx.event.ActionEvent actionEvent) throws IOException, SQLException {
-//
-//        databaseDriver.connect();
-//        //databaseDriver.getAllCourses();
-//        ArrayList<Course> courseArrayList = databaseDriver.getAllCourses();
-//        ObservableList<Course> observableCourses = FXCollections.observableArrayList(courseArrayList);
-//        System.out.println("This is in the observableList " + observableCourses.get(0).getTitle());
-//        databaseDriver.disconnect();
-//        numCol.setCellValueFactory(new PropertyValueFactory<>("courseNumber"));
-//
-//        displayCourses.setItems(observableCourses);
-//        List<Course> courseList = courseArrayList;
-//        ObservableList<Course> courses = FXCollections.observableList(courseList);
-//        displayCourses.setItems(courses);
-        //displayCourses.getItems().clear();
-//
-//        databaseDriver.connect();
-//
-//        ArrayList<Course> databaseCourses = databaseDriver.getAllCourses();
-//        databaseDriver.disconnect();
-//        String query = "SELECT * FROM course";
-//        String inputDept = subject.getText();
-//        String inputNum = courseNum.getText();
-//        String inputTitle = courseTitle.getText();
-//        for (Course course: databaseCourses){
-//            if (course.getCourseNumber().equalsIgnoreCase(inputNum) || course.getDepartment().equalsIgnoreCase(inputDept)
-//            || course.getTitle().equalsIgnoreCase(inputTitle)){
-//
-//                courses.add(course);
-//            }
-//        }
-//        displayCourses.setItems(courses);
-
-//        }
 
 }
